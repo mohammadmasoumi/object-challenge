9f9f9a6a42cd29f3506d798a94a161479cceb66f
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-21 02:51:33 +0330

    update Dockerfiles and openresty

--- deployment/data/lua/middleware.lua
+++ deployment/data/lua/middleware.lua
@@ -1,9 +1,5 @@
---local mongo = require "mongo"
---local client = mongo.Clinet("mongodb://root:1234@mongo:27017/")
---userCollection = client:getCollection("challenge", "users")
 
 local function createBucket()
---  local result = userCollection:find_one({})
   ngx.say('Hello this is mohammad')
 end
 
