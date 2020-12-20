774242f8905ab7b3089fd810d030af5cc5eaf7f6
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-20 22:00:02 +0330

    update nginx conf

--- deployment/data/lua/middleware.lua
+++ deployment/data/lua/middleware.lua
@@ -1,9 +1,9 @@
-local mongo = require "mongo"
-local client = mongo.Clinet("mongodb://root:1234@mongo:27017/")
-userCollection = client:getCollection("challenge", "users")
+--local mongo = require "mongo"
+--local client = mongo.Clinet("mongodb://root:1234@mongo:27017/")
+--userCollection = client:getCollection("challenge", "users")
 
 local function createBucket()
-  local result = userCollection:find_one({})
+--  local result = userCollection:find_one({})
   ngx.say('Hello this is mohammad')
 end
 
