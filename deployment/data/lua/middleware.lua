1d20c362f2b4baefdb4efdbe7b6a81f03a14db58
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-20 18:41:06 +0330

    update code

--- deployment/data/lua/middleware.lua
+++ deployment/data/lua/middleware.lua
@@ -1,7 +1,7 @@
-local MongoClient = require("mongorover.MongoClient")
-local client = MongoClient.new("mongodb://root:1234@mongo:27017/")
+local mongorover = require "mongorover"
+local client = mongorover.MongoClient.new("mongodb://root:1234@mongo:27017/")
 local challengeDatabase = client:getDatabase("challenge")
-local userCollection = exampleDatabase:getCollection("users")
+userCollection = challengeDatabase:getCollection("users")
 
 local function createBucket()
   local result = userCollection:find_one({})
