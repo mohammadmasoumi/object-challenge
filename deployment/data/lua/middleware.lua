27553f5b03a0cc46b490e75c60a93bd261d31d73
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-20 18:09:45 +0330

    update lua file

--- deployment/data/lua/middleware.lua
+++ deployment/data/lua/middleware.lua
@@ -1,5 +1,11 @@
-local function process()
-  ngx.say('the spice must flow')
+local MongoClient = require("mongorover.MongoClient")
+local client = MongoClient.new("mongodb://root:1234@mongo:27017/")
+local challengeDatabase = client:getDatabase("challenge")
+local userCollection = exampleDatabase:getCollection("users")
+
+local function createBucket()
+  local result = userCollection:find_one({})
+  ngx.say('Hello this is mohammad')
 end
 
-return process
+return createBucket
