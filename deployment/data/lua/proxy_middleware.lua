114ba80a9569bc208cb2ff20c831199edf2dffc8
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-22 02:50:36 +0330

    fix lua file

--- deployment/data/lua/proxy_middleware.lua
+++ deployment/data/lua/proxy_middleware.lua
@@ -4,11 +4,14 @@ local httpc = http.new()
 local challenge_url = "http://challenge:5000/" .. ngx.var.uri
 
 ngx.say(challenge_url)
+ngx.say(ngx.req.get_method())
+ngx.say(ngx.req.get_body_data())
+ngx.say(ngx.req.get_headers())
 
 --ngx.req.get_body_data()
-local res, err = https:request_uri(challenge_url, {
-    method = ngx.req.get_method(),
-    body = ngx.req.get_body_data(),
-    header = ngx.req.get_headers()
-})
-ngx.say("response is:" .. res)
+--local res, err = https:request_uri(challenge_url, {
+--    method = ngx.req.get_method(),
+--    body = ngx.req.get_body_data(),
+--    header = ngx.req.get_headers()
+--})
+--ngx.say("response is:" .. res)
