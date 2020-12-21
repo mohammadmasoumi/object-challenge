3c2a0b769872a43cce6e4321f1e68b1e3d6633a9
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-22 01:35:06 +0330

    check openresty and challenge connection

--- object_challenge/views/hello_world.py
+++ object_challenge/views/hello_world.py
@@ -10,6 +10,7 @@ logger = getLogger(__name__)
 @app.route('/')
 def hello_world():
     app.logger.info(f"users: {User.objects.filter()}")
+    User.objects.create(user_id=1, name='asghar')
     app.logger.info("-------------------------------")
     response = app.response_class(
         response=json.dumps({"result": "OK"}),
