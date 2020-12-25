91b99216d9a1a5448a0baf2a4b50db99d68d20c4
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-26 02:41:42 +0330

    update

--- object_challenge/config.py
+++ object_challenge/config.py
@@ -61,7 +61,7 @@ class TestingConfig(BaseConfig):
     TESTING = True
 
     MONGO_DB = get_env_var('MONGO_DB', default='test', prefixed=True)
-    MONGO_HOST = 'localhost'
+    MONGO_HOST = get_env_var('MONGO_HOST', default='mongo', prefixed=True)
     MONGO_PORT = int(get_env_var('MONGO_PORT', default=27017, prefixed=True))
     MONGO_USERNAME = get_env_var('MONGO_USERNAME', default='test', prefixed=True)
     MONGO_PASSWORD = get_env_var('MONGO_PASSWORD', default='test', prefixed=True)
