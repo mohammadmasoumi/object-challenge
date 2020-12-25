662c5ab9dadbd3ad4d03f6538db1061ff1e88966
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-25 19:15:38 +0330

    comment command

---
+++ object_challenge/tests/test_bucket.py
@@ -0,0 +1,13 @@
+import unittest
+
+# from project.server import db
+# from project.server.models import User
+# from project.tests.base import BaseTestCase
+
+
+# class TestAuthBlueprint(BaseTestCase):
+#     pass
+
+
+if __name__ == '__main__':
+    unittest.main()
