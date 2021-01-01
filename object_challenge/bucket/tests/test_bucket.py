7bdd0fe8b3ae6caba6ef9769263780d8f32f2318
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-01 17:59:09 +0330

    add tests

--- object_challenge/bucket/tests/test_bucket.py
+++ object_challenge/bucket/tests/test_bucket.py
@@ -15,16 +15,80 @@ def create_bucket(self, bucket, token):
     )
 
 
-class TestAuthBlueprint(BaseTestCase):
+def create_arvan(self, bucket, token):
+    return self.client.post(
+        '/arvan',
+        data=json.dumps(dict(
+            bucket=bucket,
+        )),
+        headers=dict(Authorization=f'{self.app.config["BEARER"]} {token}'),
+        content_type='application/json',
+    )
+
+
+class TestBucketAPI(BaseTestCase):
 
     def test_create_bucket(self):
-        """ Test for bucket creation"""
+        """
+        test create not assigned bucket
+        """
         with self.client:
             data = {
                 'bucket': 'test',
                 'token': 'token_1'
             }
             response = create_bucket(self, **data)
+            data = json.loads(response.data.decode())
+
+            self.assertEqual(data.get('result'), 'ok')
+            self.assertEqual(response.content_type, 'application/json')
+            self.assertEqual(response.status_code, 200)
+
+    def test_create_allowed_bucket(self):
+        """
+        test create allowed bucket
+        """
+        with self.client:
+            data = {
+                'bucket': 'arvan3',
+                'token': 'token_1'
+            }
+            response = create_bucket(self, **data)
+            data = json.loads(response.data.decode())
+
+            self.assertEqual(data.get('result'), 'ok')
+            self.assertEqual(response.content_type, 'application/json')
+            self.assertEqual(response.status_code, 200)
+
+    def test_create_not_allowed_bucket(self):
+        """
+        test create not allowed bucket
+        """
+        with self.client:
+            data = {
+                'bucket': 'pushe3',
+                'token': 'token_1'
+            }
+            response = create_bucket(self, **data)
+            data = json.loads(response.data.decode())
+
+            self.assertEqual(data.get('result'), 'not allowed')
+            self.assertEqual(response.content_type, 'application/json')
+            self.assertEqual(response.status_code, 400)
+
+
+class TestArvanAPI(BaseTestCase):
+
+    def test_create_arvan(self):
+        """
+        test create validated bucket in arvan
+        """
+        with self.client:
+            data = {
+                'bucket': 'test',
+                'token': 'token_1'
+            }
+            response = create_arvan(self, **data)
             data = json.loads(response.data.decode())
 
             self.assertEqual(data.get('result'), 'ok')
